d559c58a39b59227a6065bf0c2ba5d400d37a9c4
Oori Schubert <oorischubert@gmail.com>
2022-07-21 17:38:33 +0300

    first commit

---
+++ api.py
@@ -0,0 +1,30 @@
+import json
+from flask import Flask, jsonify, request
+
+color = [{"012245":"red"}]
+
+# creating the instance of our flask application
+app = Flask(__name__)
+
+
+@app.route('/color',methods = ['GET', 'POST'])
+#@cross_origin(maxAge = 3600) #web auth
+def downloadRoute0(): #change number for every new route!
+    global color
+    if(request.method == 'GET'):
+        color_key = request.args.get('key')
+        if(color.has_key(color_key)):
+         return jsonify({"color" : color[color_key]}) #returns color from app with code
+        else:
+            return jsonify({"color":"null"})
+
+    elif(request.method == 'POST'):
+        color_key = request.args.get('key')
+        request_data = request.data
+        request_data = json.loads(request_data.decode('utf-8'))
+        color[color_key] = request_data['color']
+        return ' '
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
