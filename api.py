c2412a9b4948381fc3db7daa45423452b071d5a7
Oori Schubert <oorischubert@gmail.com>
2022-07-21 17:45:43 +0300

    test

--- api.py
+++ api.py
@@ -13,7 +13,7 @@ def downloadRoute0(): #change number for every new route!
     global color
     if(request.method == 'GET'):
         color_key = request.args.get('key')
-        if(color.has_key(color_key)):
+        if(color[color_key] != "null"):
          return jsonify({"color" : color[color_key]}) #returns color from app with code
         else:
             return jsonify({"color":"null"})
